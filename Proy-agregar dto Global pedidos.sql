52558113af0a191646b377b225633372126d028f
Edgar Mercado <emercadogarcia@gmail.com>
2021-07-04 02:23:45 -0400

    Primeras del proyecto

---
+++ Proy-agregar dto Global pedidos.sql
@@ -0,0 +1,27 @@
+Proyecto para agregar Descuento global a pedidos especificos:
+1. se debe modificar programa VALORARPED para agregar plugin.
+2. Crear programa para lecturar los pedidos y aconndicionar para que pueda cambiar el dscto Global.
+3. Crear consulta para saber cuantas UEN estan en ese pedido.
+4. Mostrar advertencia en el programa que existen mas de una UEN en el pedeido.
+5. Agregar campo para adicionar el descuento GLOBAL que se agregará.
+6. Generar sql para aplicar al pedido y cargar el descuento.
+
+/****************************************************/
+1. se debe modificar programa VALORARPED para agregar plugin.
+Se ha realizado las modificaciones al porgrama agregando un plugin "key_g"  para llamar al programa creado.
+
+/****************************************************/
+2. Crear programa para lecturar los pedidos y aconndicionar para que pueda cambiar el dscto Global.
+Se ha creado el programa "BOL_METADATOS"  el cual esta como base para lecutrar los pedidos y agregar el descuento global.
+
+
+/****************************************************/
+3. Crear consulta para saber cuantas UEN estan en ese pedido.
+
+
+/****************************************************/
+4. Mostrar advertencia en el programa que existen mas de una UEN en el pedeido.
+/****************************************************/
+5. Agregar campo para adicionar el descuento GLOBAL que se agregará.
+/****************************************************/
+6. Generar sql para aplicar al pedido y cargar el descuento.
