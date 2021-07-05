1f00636ad180f11690632f200da0ab426c8b2121
Edgar Mercado <emercadogarcia@gmail.com>
2021-07-05 04:06:33 -0400

    se agrega codigo para agregar descuento a pedidos. este trabaja con un plugin que se agrego en el programa REVALORIZACION DE PEDIDOS.

--- Proy-agregar dto Global pedidos.sql
+++ Proy-agregar dto Global pedidos.sql
@@ -23,13 +23,30 @@ from (select pedidos_ventas_lin.*, (selEct DESCRIPCION FROM FAMILIAS f WHERE f.c
 where ejercicio=2016 and  numero_pedido=7701  and numero_serie='010' and empresa='004'
 GROUP BY UEN
 
+/***** FAMILIAS PARA OBTENER LA UEN  *****/
+SELECT CODIGO_FAMILIA,DESCRIPCION,numero_tabla, rowid
+FROM FAMILIAS
+WHERE codigo_empresa = '004' AND CODIGO_FAMILIA LIKE '0401%'
+/******     CREAR respuesta de archivo  ******/
+cursor x is select UEN,COUNT(*) CANT
+from (select pedidos_ventas_lin.*, (selEct DESCRIPCION FROM FAMILIAS f WHERE f.codigo_empresa=Pedidos_ventas_lin.EMPRESA and f.codigo_familia=(select codigo_estad5 from articulos a where a.codigo_empresa=pedidos_ventas_lin.Empresa and a.codigo_articulo=pedidos_ventas_lin.articulo) ) UEN FROM PEDIDOS_VENTAS_LIN) PL
+where ejercicio=2016 and  numero_pedido=7701  and numero_serie='010' and empresa='004'
+GROUP BY UEN
+
+cursor x is select UEN,COUNT(*) CANT
+from (select pedidos_ventas_lin.*, (selEct DESCRIPCION FROM FAMILIAS f WHERE f.codigo_empresa=Pedidos_ventas_lin.EMPRESA and f.codigo_familia=(select codigo_estad5 from articulos a where a.codigo_empresa=pedidos_ventas_lin.Empresa and a.codigo_articulo=pedidos_ventas_lin.articulo) ) UEN FROM PEDIDOS_VENTAS_LIN) PL
+where ejercicio= to_char(:b2.FECHA_PEDIDO_CHAR) and  numero_pedido=:b2.NUMERO_PEDIDO and numero_serie=b2.numero_serie and empresa=:GLOBAL.codigo_empresa
+GROUP BY UEN;
+begin
+for reg in x loop
+    :b2.UEN:= b2.uen ||
+end loop;
+
+/************************/
+
 pARA LOS LABORATORIOS: 
 SELECT codigo_familia c1, rowid rowid_lov FROM familias
 codigo_empresa = :global.codigo_empresa AND numero_tabla = 3
-
-SELECT CODIGO_FAMILIA,DESCRIPCION,numero_tabla
-FROM FAMILIAS
-WHERE codigo_empresa = '004' AND CODIGO_FAMILIA LIKE '040%'
 
 :SF:pk_va_articulos.get_campo_descripcion_fam(p_empresa=>:global.codigo_empresa,p_usuario=>:global.usuario,p_alias=>'familias'):EF: c2, 
 
